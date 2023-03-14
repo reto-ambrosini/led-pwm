b1168db0a758ef2dbe08d1c9b36aabb97128bb2d
Reto Ambrosini <reto.ambrosini@gmail.com>
2023-03-14 10:30:20 +0100

    LED_1 si accende con S_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// switch
+
+#define S_1 62
+#define S_2 63
+#define S_3 64
+#define S_4 65
+
+// LED
+
+#define LED_1 66
+#define LED_2 67
+#define LED_3 68
+#define LED_4 69
+
+
+void setup() 
+{
+  pinMode(LED_1,OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  uint8_t acceso;
+  acceso=digitalRead(S_1)^0x01;
+  digitalWrite(LED_1,acceso);
 }
