d4578f9e48aaf76fc604d7f2b109f3713c92e57b
reto ambrosini <reto.ambrosini@gmail.com>
2023-03-20 08:55:29 +0100

    aggiunto potenziometro

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,13 @@ void setup()
 void loop() 
 {
 	uint8_t acceso;
+	uint16_t valore_pot;
+	uint16_t valore_pwm;
 
 	acceso=digitalRead(S_1)^0x01;
 	digitalWrite(LED_1,acceso);
 
-	analogWrite(LED_PWM,20);
+	valore_pot=analogRead(POT_2);
+	valore_pwm=valore_pot>>2;
+	analogWrite(LED_PWM,valore_pwm);
 }
